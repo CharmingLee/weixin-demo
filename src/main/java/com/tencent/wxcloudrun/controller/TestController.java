9b88a7c38e9d1843f6f3dc7f50d8ee90f86b3f1a
charminglee <charminglee@jieztech.com>
2022-08-04 17:46:41 +0800

    1.增加忽略文件 2.增加测试接口

---
+++ src/main/java/com/tencent/wxcloudrun/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.tencent.wxcloudrun.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author CharmingLee <charminglee@jieztech.com>
+ * @date 2022/8/4 17:45
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test1")
+    public String index(){
+        return "hello";
+    }
+
+
+}
