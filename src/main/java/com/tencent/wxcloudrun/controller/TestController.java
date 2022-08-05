7d1efc012fca4f9708a9b4dd4071d840bb845d9f
charminglee <charminglee@jieztech.com>
2022-08-05 17:14:22 +0800

    增加测试公众号验证接口

--- src/main/java/com/tencent/wxcloudrun/controller/TestController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.tencent.wxcloudrun.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +14,11 @@ public class TestController {
     @GetMapping("/test1")
     public String index(){
         return "hello";
+    }
+
+    @GetMapping("/checkSignature")
+    public String checkSignature(@RequestParam(required = false) String echostr) {
+        return echostr;
     }
 
 
